a8f5b1597f9afc132f8d8a3774e41169bb2a2e7b
Jenny <pinkywafer@gmail.com>
2017-07-15 04:43:54 +0100

    Strip md extension from readme in Arrange.bat
    Allows file to be opened easily on systems which do not natively support markdown

--- Arrange.bat
+++ Arrange.bat
@@ -2,7 +2,7 @@
 mkdir 2.2\en\SYSTEM
 copy Credits.txt 2.2\Credits.txt
 copy LICENSE 2.2\LICENSE
-copy README 2.2\README
+copy README.md 2.2\README
 for /f "skip=1 usebackq tokens=2-3 delims=," %%a in ("contents.csv") do (
 	echo %%a
 	copy en%%a 2.2\en%%a
@@ -12,7 +12,7 @@ rd /s /q 2.2
 mkdir 2.1\en\SYSTEM
 copy Credits.txt 2.1\Credits.txt
 copy LICENSE 2.1\LICENSE
-copy README 2.1\README
+copy README.md 2.1\README
 for /f "skip=1 usebackq tokens=2-3 delims=," %%a in ("contents.csv") do (
 	echo %%a
 	copy en%%a 2.1\en%%b
